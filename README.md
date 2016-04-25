a0ee0cba36113667daabefd91542e8d43f3f21b2
Cordless1992 <Gcrane1992@outlook.com>
2016-04-25 22:16:14 +0100

    DarwinWC V0.2b
    Major release - implements to running versions one stable the other
    unstable for testing
    See README.MD for more details

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-README - DarwinWC V0.1b
+README - DarwinWC V0.2b
 
 "DarwinWC" is a work in progress which is aimed at finding train services which are not moving. The main purpose of "DarwinWC" is to be a backend data collecter, which can then be used for other purposes.
 
 
 Quick Start -
-On Windows download .zip, extract and then just click run.bat.
+For Stable Releases -
+On Windows download .zip, extract and then just click 
+Run_(Stable).bat
+For BETA (Unstable) Releases -
+On Windows download .zip, extract and then just click 
+Run_BETA_Unstable).bat
 
 Current Status - Ongoing (Status: B)
 
@@ -17,6 +22,7 @@ https://pypi.python.org/pypi/MySQL-python/1.2.5 - MySQLdb
 Added Files
 --------------
 Untitled2.py - The base elements of retrieving the oldest item in the database and checking it against a set amount of time then delete it if it is over the set time. Currently this only displays the difference in seconds and does nothing with the data
+Testingcopy.py - This is a BETA version of the Untitled1.py - it has the ability to only show delayed UID once per incoming message rather than each location
 
 Removed Files
 --------------
@@ -29,3 +35,4 @@ Untitled1.py - Added Time of message received
 Other Changes
 --------------
 Test completed for amount of data stored see test notes
+Changes to how the program needs to be started see "Quick Start" above
---
+++ Testingcopy.py
@@ -0,0 +1,103 @@
+#WARNING - This is an unstable testing copy and should only be used by testers
+import zlib, stomp
+import MySQLdb
+from time import gmtime, strftime
+import xml.dom.minidom
+from xml.dom.minidom import parse, parseString
+class MyListener(object):
+    def on_error(self, headers, message):
+        print('received an error %s' % message)
+
+    def on_message(self, headers, message):
+
+        decompressed_data=zlib.decompress(message, 16+zlib.MAX_WBITS)
+
+        #print('received a message %s' % decompressed_data)
+        XML = decompressed_data
+        f = open("test.xml","wb")
+        f.write(XML)
+        f.close()
+        dom = parse("test.xml")
+        a = "r"        
+        #Example <uR updateOrigin="Darwin"><TS rid="201604221223687" ssd="2016-04-22" uid="W05044">
+        
+        for node1 in dom.getElementsByTagName('uR'):
+            x = node1.attributes["updateOrigin"]      #Information Source
+            
+        for node2 in dom.getElementsByTagName('TS'):
+            y = node2.attributes["uid"]               #UID
+            
+        for node in dom.getElementsByTagName('ns3:Location'):
+            o = node.attributes["tpl"]               #TIPLOC Information
+            #print y.value, o.value, x.value
+
+            #print XML for testing purposes (if commented its off)
+
+        db = MySQLdb.connect("sql8.freesqldatabase.com","sql8116670","u47HNmCYr8","sql8116670")
+        cursor = db.cursor()
+        sql = "(SELECT * FROM TRAINS ORDER BY TME DESC LIMIT 1)"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            L = row[1]
+        db.close()
+        #print y.value
+        #print L
+        try:
+            for node3 in dom.getElementsByTagName('ns3:arr'):
+                d = node3.attributes["delayed"]
+                #Store values of data in a, b and c variables
+            a = y.value
+            b = o.value
+            c = x.value
+            t = strftime("%H:%M:%S", gmtime())
+            print "Received at: ", t, "UID: ", a, " TIPLOC: ", b, " Source: ", c, " Status: Delayed"
+        except KeyError:
+            #print "Error"
+            a = L
+            pass
+        
+        if a == L:
+            #print "Already stored or not delayed"
+        else:
+            #Open DB connection (change as required)
+                db = MySQLdb.connect("sql8.freesqldatabase.com","sql8116670","u47HNmCYr8","sql8116670")
+                cursor = db.cursor()
+
+            #Prepare information to be insered
+                sql = "INSERT INTO TRAINS(TME, UID, TPL, SOU) VALUES (%s,%s,%s,%s)"
+    
+            #Commit changes to database
+                try:
+            #("select freq from matrix_brown where a_id in (?) and b_id in (?)", (b_item_id,b_after_id))
+            #print "Starting Execute"
+                    cursor.execute(sql, (t,a,b,c))
+            #print "Passed Execute"
+                    db.commit()
+            #countera = countera  + 1
+            #print countera
+            #print "Written to Database"
+                except Exception,e:
+                    print e
+                    db.rollback()
+            #print "Failed to Write to Database"
+                db.close()
+
+        #skip if nothing found in ns3:arrived/delayed attribute        
+                         #print XML
+
+conn = stomp.Connection([("datafeeds.nationalrail.co.uk", 61613)])
+
+conn.set_listener('', MyListener())
+
+conn.start()
+
+conn.connect("d3user", "d3password")
+
+QUEUE = "D379de05de-a524-4030-a862-a1f611eda684" # Your queue name here
+
+conn.subscribe("/queue/"+QUEUE, id=1, ack='auto')
+
+while 1:
+
+    pass
